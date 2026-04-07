346ea701d52c54e5d423c93630f7c7f9f4ea832c
Anmolxx <anmoldeepkaur1103@gmail.com>
2026-04-07 15:37:16 +0530

    fix: correct Supabase public URL format

--- src/files/infrastructure/uploader/s3/files.service.ts
+++ src/files/infrastructure/uploader/s3/files.service.ts
@@ -1,3 +1,35 @@
+// // import {
+// //   HttpStatus,
+// //   Injectable,
+// //   UnprocessableEntityException,
+// // } from '@nestjs/common';
+// // import { FileType } from '../../../domain/file';
+// // import { FileStatus } from '../../../file.enum';
+// // import { FileRepository } from '../../persistence/file.repository';
+
+// // @Injectable()
+// // export class FilesS3Service {
+// //   constructor(private readonly fileRepository: FileRepository) {}
+
+// //   async create(file: Express.MulterS3.File): Promise<{ file: FileType }> {
+// //     if (!file) {
+// //       throw new UnprocessableEntityException({
+// //         status: HttpStatus.UNPROCESSABLE_ENTITY,
+// //         errors: {
+// //           file: 'selectFile',
+// //         },
+// //       });
+// //     }
+
+// //     return {
+// //       file: await this.fileRepository.create({
+// //         path: `${process.env.AWS_S3_ENDPOINT}/object/public/${process.env.AWS_DEFAULT_S3_BUCKET}/${file.key}`,
+// //         status: FileStatus.TEMPORARY,
+// //       }),
+// //     };
+// //   }
+// // }
+
 // import {
 //   HttpStatus,
 //   Injectable,
@@ -21,9 +53,13 @@
 //       });
 //     }
 
+//     console.log('file.key:', file.key, 'file.location:', file.location);
+
+//     const supabasePublicUrl = `https://tnpwudwthsicppibhqzp.supabase.co/storage/v1/object/public/${process.env.AWS_DEFAULT_S3_BUCKET}/${file.key}`;
+
 //     return {
 //       file: await this.fileRepository.create({
-//         path: `${process.env.AWS_S3_ENDPOINT}/object/public/${process.env.AWS_DEFAULT_S3_BUCKET}/${file.key}`,
+//         path: supabasePublicUrl,
 //         status: FileStatus.TEMPORARY,
 //       }),
 //     };
@@ -47,19 +83,16 @@ export class FilesS3Service {
     if (!file) {
       throw new UnprocessableEntityException({
         status: HttpStatus.UNPROCESSABLE_ENTITY,
-        errors: {
-          file: 'selectFile',
-        },
+        errors: { file: 'selectFile' },
       });
     }
 
-    console.log('file.key:', file.key, 'file.location:', file.location);
-
-    const supabasePublicUrl = `https://tnpwudwthsicppibhqzp.supabase.co/storage/v1/object/public/${process.env.AWS_DEFAULT_S3_BUCKET}/${file.key}`;
+    const key = file.key.split('/').pop() ?? file.key;
+    const path = `https://tnpwudwthsicppibhqzp.supabase.co/storage/v1/object/public/i-love-memes-files/${key}`;
 
     return {
       file: await this.fileRepository.create({
-        path: supabasePublicUrl,
+        path,
         status: FileStatus.TEMPORARY,
       }),
     };
