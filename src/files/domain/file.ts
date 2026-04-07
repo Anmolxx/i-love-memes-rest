82e06fd78dcf590ab88f50449e74acb2d4c9dc3f
Anmolxx <anmoldeepkaur1103@gmail.com>
2026-04-08 00:26:38 +0530

    fix: remove S3 presigned transform, return clean URL

--- src/files/domain/file.ts
+++ src/files/domain/file.ts
@@ -1,12 +1,6 @@
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Transform } from 'class-transformer';
 import { Allow } from 'class-validator';
-import { AppConfig } from '../../config/app-config.type';
-import appConfig from '../../config/app.config';
-import { FileConfig, FileDriver } from '../config/file-config.type';
-import fileConfig from '../config/file.config';
 import { FileStatus } from '../file.enum';
 
 export class FileType {
@@ -21,37 +15,7 @@ export class FileType {
     type: String,
     example: 'https://example.com/path/to/file.jpg',
   })
-  @Transform(
-    ({ value }) => {
-      if ((fileConfig() as FileConfig).driver === FileDriver.LOCAL) {
-        return (appConfig() as AppConfig).backendDomain + value;
-      } else if (
-        [FileDriver.S3_PRESIGNED, FileDriver.S3].includes(
-          (fileConfig() as FileConfig).driver,
-        )
-      ) {
-        const s3 = new S3Client({
-          region: (fileConfig() as FileConfig).awsS3Region ?? '',
-          credentials: {
-            accessKeyId: (fileConfig() as FileConfig).accessKeyId ?? '',
-            secretAccessKey: (fileConfig() as FileConfig).secretAccessKey ?? '',
-          },
-        });
-
-        const command = new GetObjectCommand({
-          Bucket: (fileConfig() as FileConfig).awsDefaultS3Bucket ?? '',
-          Key: value,
-        });
-
-        return getSignedUrl(s3, command, { expiresIn: 3600 });
-      }
-
-      return value;
-    },
-    {
-      toPlainOnly: true,
-    },
-  )
+  @Transform(({ value }) => value, { toPlainOnly: true })
   path: string;
 
   @Exclude()
