363af2360d765889ac935f1e840c399d03f88b43
Anmolxx <anmoldeepkaur1103@gmail.com>
2026-04-07 22:37:25 +0530

    test: sentinel value to confirm deployment

--- src/files/infrastructure/uploader/s3/files.service.ts
+++ src/files/infrastructure/uploader/s3/files.service.ts
@@ -20,7 +20,7 @@ export class FilesS3Service {
     }
 
     const cleanKey = file.key.split('?')[0].split('/').pop() ?? file.key;
-    const path = `https://${process.env.AWS_DEFAULT_S3_BUCKET}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${cleanKey}`;
+    const path = `TESTSENTINEL_https://${process.env.AWS_DEFAULT_S3_BUCKET}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${cleanKey}`;
 
     return {
       file: await this.fileRepository.create({
